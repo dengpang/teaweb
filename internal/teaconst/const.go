0d0f1c0cc31837fa1b656f44ab9f5be56f8f3972
dengpang <404821634@qq.com>
2022-07-14 10:15:19 +0800

    捷网logo

--- internal/teaconst/const.go
+++ internal/teaconst/const.go
@@ -4,6 +4,6 @@ const (
 	TeaVersion = "1.5.0"
 
 	TeaProcessName = "teaweb" // 进程名
-	TeaProductName = "智安云监控"  // 产品名
+	TeaProductName = "捷网云监控"  // 产品名
 	TeaProductCode = "TeaWeb" // 产品代号，会用在一些对外访问的User-Agent里面，一定要是英文
 )
